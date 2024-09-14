b4fe2323b88c2dfb95928eeac85c33e3ab17a200
Kunal-Kumar-Sahoo <kunal.sahoo2003@gmail.com>
2024-09-14 20:46:40 +0530

    Added the forward pass functionality

--- src/model.py
+++ src/model.py
@@ -25,7 +25,7 @@ class MoleculeNet(nn.Module):
         self.conv_layers = nn.ModuleList([])
         self.transf_layers = nn.ModuleList([])
         self.pooling_layers = nn.ModuleList([])
-        self.bn_Layers = nn.ModuleList([])
+        self.bn_layers = nn.ModuleList([])
 
         # Transformation layers
         self.conv1 = gnn.TransformerConv(feature_size,
@@ -48,7 +48,7 @@ class MoleculeNet(nn.Module):
                                     edge_dim=edge_dim,
                                     beta=True))
             self.transf_layers.append(nn.Linear(embedding_size * n_heads, embedding_size))
-            self.bn_Layers.append(nn.BatchNorm1d(embedding_size))
+            self.bn_layers.append(nn.BatchNorm1d(embedding_size))
 
             if i % self.top_k_every_n == 0:
                 self.pooling_layers.append(gnn.TopKPooling(embedding_size, ratio=top_k_ratio))
@@ -59,5 +59,36 @@ class MoleculeNet(nn.Module):
         self.linear3 = nn.Linear(dense_neurons // 2, 1)
 
     def forward(self, x, edge_attr, edge_index, batch_index):
-        pass
+        # Initial transformation
+        x = self.conv1(x, edge_index, edge_attr)
+        x = torch.relu(self.transf1(x))
+        x = self.bn1(x)
 
+        # Holds the intermediate graph representations
+        global_representations = []
+
+        for i in range(self.n_layers):
+            x = self.conv_layers[i](x, edge_index, edge_attr)
+            x = torch.relu(self.transf_layers[i](x))
+            x = self.bn_layers[i](x)
+
+            # Aggregate the last layer
+            if i % self.top_k_every_n == 0 or i == self.n_layers:
+                x, edge_index, edge_attr, batch_index, _, _ = self.pooling_layers[i // self.top_k_every_n](
+                    x, edge_index, edge_attr, batch_index)
+                # Add current representations
+                global_representations.append(torch.cat([
+                    gnn.global_max_pool(x, batch_index),
+                    gnn.global_mean_pool(x, batch_index)
+                ], dim=1))
+        
+        x = sum(global_representations)
+
+        # Output block
+        x = torch.relu(self.linear1(x))
+        x = F.dropout(x, p=0.8, training=self.training)
+        x = torch.relu(self.linear2(x))
+        x = F.dropout(x, p=0.8, training=self.training)
+        x = self.linear3(x)
+
+        return x
